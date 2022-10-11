4a1f970dcf3f2518b2b27ead33eac11e7d6ee2f4
Joseph Watkins <102884938+Jwatk13@users.noreply.github.com>
2022-10-11 14:51:58 -0400

    added more images and styles to landing page

--- src/components/pages/Landing/RenderLandingPage.js
+++ src/components/pages/Landing/RenderLandingPage.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Typography } from 'antd';
 import navLogo from '../../../images/navLogo.jpg';
+import ghibli2 from '../../../images/ghibli2.jpg';
 
 const { Title, Paragraph } = Typography;
 
 function RenderLandingPage(props) {
     return (
         <div className='landing-container'>
-            <div className="landing-container">
+            <div className='landpage-background'>
+                <img className='landing-primaryImg' src={ghibli2} alt="background img" />
+            </div>
+            <div className='landing-text'>
                 <Title className="landing-main-text landing-main-title">
                     Hello world!
                 </Title>
