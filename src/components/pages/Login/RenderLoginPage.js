78cb8263daddd30bb0c26ffa42fca764d9da659a
Joseph Watkins <102884938+Jwatk13@users.noreply.github.com>
2022-10-11 18:05:15 -0400

    login form style and setup

--- src/components/pages/Login/RenderLoginPage.js
+++ src/components/pages/Login/RenderLoginPage.js
@@ -1,8 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import laputiaCastle from '../../../images/laputiaCastle.jpg';
 
-function RenderLoginPage() {
+const intialFormValues = {
+    username: '',
+    password: '',
+}
+
+function RenderLoginPage(props) {
+    const [ values, setValues ] = useState(intialFormValues);
+
+    const { login } = props;
+
+    const onChange = evt => {
+        const { id, value } = evt.target
+        setValues({...values, [id]: value});
+    }
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        login(values)
+    }
+
+    const isDisabled = () => {
+        if (values.username.trim().length >= 3 && values.password.trim().length >= 8) {
+            return false
+        } else {
+            return true;
+        }
+    }
+  
     return (
-        <h1>Hello world</h1>
+        <div className='login-wrapper'>
+            <img className='login-background' src={laputiaCastle} alt="login background" />
+            <form className='login-form' id='loginForm' onSubmit={onSubmit}>
+                <h2>LOGIN</h2>
+                <input 
+                    maxLength={20}
+                    value={values.username}
+                    onchange={onChange}
+                    placeholder="Username"
+                    id="username"
+                />
+                <input 
+                    maxLength={28}
+                    value={values.password}
+                    onChange={onChange}
+                    placeholder="Password"
+                    id="password"
+                />
+                <button disabled={isDisabled()} id="submit" className='submit-btn'>Submit</button>
+            </form>
+        </div>
     );
 }
 
